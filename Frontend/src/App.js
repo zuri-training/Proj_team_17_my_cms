a6464d07b3ad47d754285a9ca7163616f32c452a
Nkiriobasi <obasinkiri@gmail.com>
2022-08-07 07:18:00 +0100

    removed the grapesjs customization funtionality

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,5 @@
 import { Routes, Route, } from "react-router-dom";
 
-import GrapesjsMain from "./components/GrapesjsMain";
-
 import LandingPage from "./pages/LandingPage";
 import ForgotPassword from "./pages/ForgotPassword";
 import SignUp from "./pages/SignUp";
@@ -19,7 +17,6 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="contact-us" element={<ContactUs />} />
         <Route path="dashboard" element={<DashBoard/>} />
-        <Route path="grapesjs" element={<GrapesjsMain />} />
       </Routes>
   );
 }
