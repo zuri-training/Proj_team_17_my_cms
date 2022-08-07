dad4edfe8cbc008b251c94e4e97c2ba38f56148f
Nkiriobasi <obasinkiri@gmail.com>
2022-08-07 09:43:10 +0100

    completed the 404 page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,17 +6,19 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login"
 import ContactUs from "./pages/ContactUs";
 import DashBoard from "./pages/DashBoard";
+import Error from "./pages/Error";
 
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/team17cms" element={<LandingPage />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="login" element={<Login />} />
         <Route path="contact-us" element={<ContactUs />} />
         <Route path="dashboard" element={<DashBoard/>} />
+        <Route path="error" element={<Error/>} />
       </Routes>
   );
 }
