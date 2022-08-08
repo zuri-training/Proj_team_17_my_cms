a15627dd1aa0e5228dc30cfa742ccd8384f08ec8
Alafia-Ojo <ojoalafiadavid@gmail.com>
2022-08-08 16:53:37 +0100

    feed back page, about-us page no-content page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,19 +6,30 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login"
 import ContactUs from "./pages/ContactUs";
 import DashBoard from "./pages/DashBoard";
+import DashBoardSideBarPages from "./components/layout/DashBoardSideBarPages";
+import AboutUs from "./pages/AboutUs";
+import FeedBackPage from "./pages/FeedBackPage";
+
 
 
 function App() {
   return (
+    <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="login" element={<Login />} />
-        <Route path="contact-us" element={<ContactUs />} />
-        <Route path="dashboard" element={<DashBoard/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/dashboard" element={<DashBoard/>} />
+        <Route path="/about-us" element={<AboutUs/>} />
+        <Route path="/feedback-hub" element={<FeedBackPage/>} />
+       
+        
       </Routes>
-  );
+      <DashBoardSideBarPages/>
+</>
+  ) 
 }
 
 export default App;
