e6ee631fba912632bc96ad9ae41833939e55bac4
Nkiriobasi <obasinkiri@gmail.com>
2022-08-09 18:40:02 +0100

    added a background image and modified the width

--- Frontend/src/pages/ForgotPassword.js
+++ Frontend/src/pages/ForgotPassword.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import forgotp from './../assets/img/forgotp-img.png';
 
 
 const ForgotPassword = () => {
   return (
     <div className='h-screen flex'>
-        <div className="bg-gray-400 w-2/4  bg-uu"></div>
+        <div className="w-2/4 flex flex-col my-auto items-center justify-center bg-uu">
+            <img src={forgotp} alt="img" className='forgotp-img' />
+        </div>
         <div className='w-1/2 flex flex-col justify-center m-auto'>
             <h1 className="text-center mb-7 font-semibold overflow-hidden text-4xl">Forgot Password?</h1>
             <p className='text-center mb-9 not-italic text-base'>Oops! <br/> Don’t worry, we’ll help you get back in.</p>
@@ -16,10 +19,11 @@ const ForgotPassword = () => {
                             type="email"
                             id="forg-passw-input"
                             placeholder="johndoe@example.com"
-                            className=" h-12 rounded-md bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-gray-500 block w-full p-2.5 pr-20 dark:placeholder-gray-400 dark:text-white " required
+                            className=" h-12 rounded-md bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-gray-500 block w-full p-2.5 pr-20" 
+                            required
                         />
                     </div>
-                    <button type="submit" className="btn-bg rounded-md font-semibold w-[481px] h-12 bg-gray-400 mt-6 flex items-center justify-center">
+                    <button type="submit" className="colorbtn-bg rounded-md font-semibold w-[481px] h-12 bg-gray-400 mt-6 flex items-center justify-center">
                         Send password recovery link
                     </button>
                 </form>
